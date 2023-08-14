12d0c8aaaeb23c0350ac8c0c4010e4912c1b3089
Iamneo <techadmin@iamneo.ai>
2023-08-14 14:35:13 +0000

    Backup Commit

---
+++ springapp/target/classes/data.sql
@@ -0,0 +1,4 @@
+-- Insert sample data for testing
+
+INSERT INTO task (task_id, task_holder_name, task_date, task_name, task_status)
+VALUES ('12211', 'Gowthaman M', '4/15/2021', 'Spring Projects', 'In Progress');
